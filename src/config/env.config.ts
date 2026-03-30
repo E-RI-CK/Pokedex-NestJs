61473fdee1e04acb71cac248986ae355b11a4f6d
Andro Valero <74332121+E-RI-CK@users.noreply.github.com>
2026-03-30 17:00:45 -0500

    fix: port number change from fixed to dynamic

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,5 +3,5 @@
 export const EnvConfiguration = () => ({
     environment: process.env.NODE_ENV || 'env',
     mongodb: process.env.MONGODB,
-    port: process.env.PORT || 3002
+    port: process.env.PORT || 3001
 })
--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
       enableImplicitConversion: true, // Transform Query Parameters
     }
   }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT!);
 }
 bootstrap();
